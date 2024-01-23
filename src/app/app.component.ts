a912f90845cc3a7f69aa1d10119ee642f99aeb92
Kandarp Patel <kandarp.patel@autodesk.com>
2024-01-23 08:51:29 -0500

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,68 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, lastValueFrom, map, of, switchMap } from 'rxjs';
+import { FormControl } from '@angular/forms';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'stock-app-angular';
+  constructor(private http: HttpClient) {
+    this.filteredOptions = this.formControl.valueChanges.pipe(
+      switchMap((val) => {
+        if (!val) return of(null);
+        return this.getSuggestions(val);
+      }),
+      map((response) => {
+        if (!response) return [];
+        return response.tickers;
+      })
+    );
+  }
+
+  formControl = new FormControl();
+  options = [];
+  autoValue = '';
+  filteredOptions: Observable<string[]>;
+
+  selectedTickers: { ticker: string; price: number }[] = [];
+
+  getSuggestions(query: string) {
+    return this.http.get<any>(
+      `https://us-west2-csci201-376723.cloudfunctions.net/stocks/v1/search?query=` +
+        query
+    );
+  }
+
+  getPrice(query: string) {
+    return this.http.get<any>(
+      `https://us-west2-csci201-376723.cloudfunctions.net/stocks/v1/price?ticker=` +
+        query
+    );
+  }
+
+  onSelectionChange(event: MatAutocompleteSelectedEvent) {
+    this.getPrice(event.option['value'] as string).subscribe((response) => {
+      this.selectedTickers.push({
+        ticker: event.option['value'] as string,
+        price: response.price,
+      });
+      this.formControl.reset('');
+    });
+  }
+
+  async refreshPrice() {
+    const newSelectedTickers = [];
+    for (const tickerObj of this.selectedTickers) {
+      const response = await lastValueFrom(this.getPrice(tickerObj.ticker));
+      newSelectedTickers.push({
+        ticker: tickerObj.ticker,
+        price: response.price,
+      });
+    }
+    this.selectedTickers = newSelectedTickers;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
